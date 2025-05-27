e3fc5cf71fad0da71fec6e5b4b0474551c1093ab
mohammed19431 <johndoe@example.commohammedmunir175@gmail.com>
2025-05-27 18:50:41 +0200

    merge

---
+++ assets/javascripts/hamburger.js
@@ -0,0 +1,9 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+  });
